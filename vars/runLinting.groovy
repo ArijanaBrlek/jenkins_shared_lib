20a97f0bf32da21d4da8f3bff4d2b3b2e12e5431
Arijana Brlek <arijanabrlek@reversinglabs.com>
2018-07-30 16:58:40 +0200

    Modify pipeline

--- vars/runLinting.groovy
+++ vars/runLinting.groovy
@@ -1,9 +1,9 @@
 
 def call(lintDirectories){
-    sh "pylint --output-format=parseable ${lintDirectories.join(' ')} >> pylint.log || exit 0"
-    // sh "pylint --output-format=parseable ${lintDirectories.join(' ')} > pylint_test.log || exit 0"
+    sh "pylint --output-format=parseable ${hello_world} >> pylint.log || exit 0"
+    sh "pylint --output-format=parseable ${hello_world_2} > pylint_test.log || exit 0"
     sh 'cat pylint.log'
-    // sh 'cat pylint_test.log'
+    sh 'cat pylint_test.log'
 
     step([
         $class: 'WarningsPublisher',
