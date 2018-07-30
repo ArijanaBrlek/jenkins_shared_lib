adf8662e31ccdcf5fdc563da1bbfdf9795eaf1c0
Arijana Brlek <arijanabrlek@reversinglabs.com>
2018-07-30 16:59:34 +0200

    Modify pipeline

--- vars/runLinting.groovy
+++ vars/runLinting.groovy
@@ -1,7 +1,7 @@
 
 def call(lintDirectories){
-    sh "pylint --output-format=parseable ${hello_world} >> pylint.log || exit 0"
-    sh "pylint --output-format=parseable ${hello_world_2} > pylint_test.log || exit 0"
+    sh "pylint --output-format=parseable hello_world >> pylint.log || exit 0"
+    sh "pylint --output-format=parseable hello_world_2 > pylint_test.log || exit 0"
     sh 'cat pylint.log'
     sh 'cat pylint_test.log'
 
